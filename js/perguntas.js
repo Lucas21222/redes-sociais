fd745595f6e7bfed2764a64070bbeeaee6e7d8cc
Lucas21222 <lucas.sobreda.bertoluci@escola.pr.gov.br>
2024-10-22 07:35:22 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,121 @@
+export const perguntas = [
+    {
+        enunciado: "Qual foi o time que ganhou a copa de 2022?",
+        alternativas: [
+            {
+                texto: "Argentina",
+                afirmacao: [
+                    "Sim, você acertou."
+                    ],
+                proxima: 1,
+            },
+            {
+                texto: "França",
+                afirmacao: [
+                    "Não, você errou."
+                    ],
+                proxima: 1,
+            },
+        ]
+    },
+   
+    {
+        enunciado: "Qual time ganhou a copa de 2014?",
+        alternativas: [
+            {
+                texto:"Argentina",
+                afirmacao: [
+                    "Não, você errou."
+                    ],
+                proxima: 2,
+            },
+            {
+                texto: "Alemanha",
+                afirmacao: [
+                    "Sim, você acertou."
+                    ],
+                proxima: 2,
+            },
+        ]
+    },
+   
+    {
+        enunciado: "qual time tem mais copa do  mundo?",
+        alternativas: [
+            {
+                texto:"Brasil",
+                afirmacao: [
+                    "Sim, esse time possui 5 copas.,"
+                    ],
+                proxima: 3,
+            },
+            {
+                texto:"Itália",
+                afirmacao: [
+                    "Não, esse time possui apenas 3 copas., "
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+   
+    {
+        enunciado: "Qual time foi campeão da copa de 2018?",
+        alternativas: [
+            {
+                texto: "Croácia",
+                afirmacao: [
+                    "Não, você errou., "
+                    ],
+                proxima: 4,
+            },
+            {
+                texto: "França",
+                afirmacao: [
+                    "Sim, você acertou.,"
+                ],
+                proxima: 4,
+            },    
+        ]
+    },
+   
+    {
+        enunciado: "Qual é o maior artilheiro da história da Copa do Mundo ?",
+        alternativas: [
+            {
+                texto: "Miroslav Klose",
+                afirmacao: [
+                    ". Correto "
+                    ],
+                proxima: 5,
+            },
+            {
+                texto: "Ronaldo Fenômeno ",
+                afirmacao: [
+                    ". Incorreto "
+                    ],
+                proxima: 5,
+            },
+        ]
+    },
+   
+    {
+        enunciado: "Qual foi o melhor goleiro do mundo da Copa de 2022 ?",
+        alternativas: [
+            {
+                texto: "Emiliano Martínez",
+                afirmacao: [
+                    " Correto "
+                    ]
+            },
+            {
+                texto: "Alisson Becker",
+                afirmacao: [
+                    " Incorreto "
+                    ]
+            },  
+        ]
+    },
+];
+
 
